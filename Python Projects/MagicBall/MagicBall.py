fc43c2a06b0a3b33761d914b6f6fb7f50a166d25
AaronTheNerd <bargeaaron@gmail.com>
2019-02-15 23:52:41 -0500

    Made improvements to MagicBall

--- Python Projects/MagicBall/MagicBall.py
+++ Python Projects/MagicBall/MagicBall.py
@@ -1,13 +1,16 @@
 # Written by Aaron Barge
 # Copyright 2019
-import io, random
+import io, random, os
 answers = []
+file_path = os.path.abspath(__file__) + "/../Answers.txt"
 def getAnswers():
+  global file_path
   global answers
   try:
-    file = open("C:/Users/AaronTheNerd/Documents/Personal-Coding-Projects/Python Projects/MagicBall/Answers.txt", 'r')
+    file = open(file_path, 'r')
     for line in file:
       answers.append(line)
+    file.close()
     return True
   except:
     print("Answers failed to load")
@@ -22,14 +25,22 @@ def getAnswer():
       getAnswer()
 
 def main():
+  global file_path
   while True:
     print("Concentrate on your Question")
     print("Type 'no' if you no longer need my help")
+    print("Type 'idea' if you have a good idea for an answer")
     text = input("Press enter when ready ")
-    if text != 'no':
-      getAnswer()
-    else:
+    if text == 'no':
       break
+    elif text == 'idea':
+      new_answer = input("What is your idea? ")
+      file = open(file_path, 'a')
+      file.write(new_answer + '\n')
+      file.close()
+      print("That is a brilliant idea!\n")
+    else:
+      getAnswer()
 
 if __name__ == "__main__":
   main()
