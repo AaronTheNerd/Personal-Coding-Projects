027d31d0c58c5c11e4d6c248661bde0b2040f1bc
Aaron Barge <bargeaaron@gmail.com>
2019-03-18 08:59:29 -0400

    VSCode changes

--- Python Projects/SciPy/SciPy.py
+++ Python Projects/SciPy/SciPy.py
@@ -1,1 +1,12 @@
-import SciPy
+e = 2.71828
+pi = 3.141529
+i = complex(0,1)
+def exp(x):
+  result = e**x
+  if type(result) is complex:
+    result = complex(round(result.real, 2), round(result.imag, 2))
+  else:
+    result = round(result, 2)
+  print("e ^(" + str(x) + ") = " + str(result))
+  return result
+exp(pi * i)
