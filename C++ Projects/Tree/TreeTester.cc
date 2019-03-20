1bf037411a2506454d706aad960be76ff9b5e95f
Aaron Barge <bargeaaron@gmail.com>
2019-03-19 20:38:53 -0400

    Fleshed out BST test

--- C++ Projects/Tree/TreeTester.cc
+++ C++ Projects/Tree/TreeTester.cc
@@ -2,20 +2,57 @@
 // Copyright 2019
 #ifndef _TREE_TESTING_CC_
 #define _TREE_TESTING_CC_
+#include <iostream>
 #include "Tree.h"
 #include "BinaryTree.h"
 #include "DoublyLinkedTree.h"
-#include "Hoffman.h"
+#include "Huffman.h"
 #endif
 
-bool testBinaryTree(BinaryTree<int> t);
-bool testDoublyLinkedTree(Tree<int> t);
-bool testHoffmanTree(Tree<int>);
+bool testBinaryTree(Tree<int>* t);
+//bool testDoublyLinkedTree(Tree<int>* t);
+//bool testHuffmanTree(Tree<int>* t);
 
 int main(int argc, char** argv) {
-  BinaryTree<int> BST;
-  testBinaryTree(BST);
+  Tree<int>* Tree = new BinaryTree<int>();
+  std::cout << "Testing Binary Search Tree" << std::endl;
+  if (!testBinaryTree(Tree))
+    std::cout << "Binary Search Tree FAILED" << std::endl;
+  return 0;
 }
-bool testBinaryTree(BinaryTree<int> t) {
-  
+bool testBinaryTree(Tree<int>* t) {
+  t->Add(5);
+  t->PrintTop();
+  t->Add(4);
+  t->PrintTop();
+  t->Add(19);
+  t->PrintTop();
+  t->Add(2);
+  t->Add(7);
+  t->Add(12);
+  t->Add(1);
+  t->Add(3);
+  t->Add(10);
+  t->Add(6);
+  t->Print();
+  if (!t->Contains(5))
+    return false;
+  if (t->Contains(100))
+    return false;
+  t->Remove(10);
+  t->Print();
+  if (t->Contains(10))
+    return false;
+  t->Remove(2);
+  t->Print();
+  if (t->Contains(2))
+    return false;
+  t->Remove(100);
+  t->Print();
+  int old_root = t->Pop();
+  t->PrintTop();
+  t->Print();
+  if (t->Contains(old_root))
+    return false;
+  return true;
 }
