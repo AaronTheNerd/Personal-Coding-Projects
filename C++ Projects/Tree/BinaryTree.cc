b667788d3f01a978f0ee5b07c8e0c5f4919d8b08
Aaron Barge <bargeaaron@gmail.com>
2019-03-19 20:37:40 -0400

    'Finished' BST

--- C++ Projects/Tree/BinaryTree.cc
+++ C++ Projects/Tree/BinaryTree.cc
@@ -6,12 +6,30 @@
 #ifndef _TREE_BINARY_SEARCH_TREE_CC_
 #define _TREE_BINARY_SEARCH_TREE_CC_
 #include "BinaryTree.h"
+#include <iostream>
 #endif
-template <class T>
-using BTSNode = BinaryTree<T>::Node;
+
 template <class T>
 BinaryTree<T>::BinaryTree() {
   this->root = NULL;
+}
+template <class T>
+BinaryTree<T>::Node::Node() {
+  this->val = 0;
+  this->left = NULL;
+  this->right = NULL;
+}
+template <class T>
+BinaryTree<T>::Node::Node(T val) {
+  this->left = NULL;
+  this->right = NULL;
+  this->val = val;
+}
+template <class T>
+BinaryTree<T>::Node::Node(T val, BinaryTree<T>::Node* left, BinaryTree<T>::Node* right) {
+  this->left = left;
+  this->right = right;
+  this->val = val;
 }
 template <class T>
 void BinaryTree<T>::Add(T val_) {
@@ -21,7 +39,7 @@ void BinaryTree<T>::Add(T val_) {
     Insert(this->root, val_);
 }
 template <class T>
-BTSNode* BinaryTree<T>::Insert(BinaryTree<T>::Node* current, T val) {
+typename BinaryTree<T>::Node* BinaryTree<T>::Insert(BinaryTree<T>::Node* current, T val) {
   if (current == NULL)
     current = new Node(val);
   else if (val < current->val)
@@ -35,7 +53,7 @@ void BinaryTree<T>::Remove(T val) {
   this->root = Remove(this->root, val);
 }
 template <class T>
-BinaryTree<T>::Node* BinaryTree<T>::Remove(BinaryTree<T>::Node* current, T val) {
+typename BinaryTree<T>::Node* BinaryTree<T>::Remove(BinaryTree<T>::Node* current, T val) {
   if (current == NULL)
     return NULL;
   if (val < current->val)
@@ -47,7 +65,7 @@ BinaryTree<T>::Node* BinaryTree<T>::Remove(BinaryTree<T>::Node* current, T val) 
       return current->left;
     if (current->left == NULL)
       return current->right;
-    BinaryTree<T>::Node* temp = new Node(current->val, current->left, current->right_child_);
+    BinaryTree<T>::Node* temp = new Node(current->val, current->left, current->right);
     current = FindMinFrom(current->right);
     current->right = RemoveMinFrom(temp->right);
     current->left = temp->left;
@@ -55,6 +73,25 @@ BinaryTree<T>::Node* BinaryTree<T>::Remove(BinaryTree<T>::Node* current, T val) 
   }
   return current;
 }
+template <class T>
+typename BinaryTree<T>::Node* BinaryTree<T>::FindMinFrom(BinaryTree<T>::Node* current) {
+  if (current == NULL)
+    return NULL;
+  if (current->left == NULL)
+    return current;
+  else
+    return FindMinFrom(current->left);
+}
+template <class T>
+typename BinaryTree<T>::Node* BinaryTree<T>::RemoveMinFrom(BinaryTree<T>::Node* current) {
+  if (current == NULL)
+    return NULL;
+  if (current->left == NULL)
+    return current->right;
+  current->left = RemoveMinFrom(current->left);
+  return current;
+}
+
 template <class T>
 bool BinaryTree<T>::Contains(T val) const {
   return Contains(this->root, val);
@@ -79,8 +116,15 @@ T BinaryTree<T>::Top() const {
     return NULL;
 }
 template <class T>
+T BinaryTree<T>::Pop() {
+  int ret_val = root->val;
+  Remove(ret_val);
+  return ret_val;
+}
+template <class T>
 void BinaryTree<T>::Print() const {
   this->PrintInOrder(this->root);
+  std::cout << std::endl;
 }
 template <class T>
 void BinaryTree<T>::PrintInOrder(BinaryTree<T>::Node* curr) const {
@@ -89,4 +133,26 @@ void BinaryTree<T>::PrintInOrder(BinaryTree<T>::Node* curr) const {
   PrintInOrder(curr->left);
   std::cout << curr->val << std::endl;
   PrintInOrder(curr->right);
+}
+template <class T>
+void BinaryTree<T>::PrintTop() const {
+  std::cout << "The address of the root is: " << this->root << std::endl;
+  if (this->root) {
+    std::cout << "  And its value is: " << this->root->val << std::endl;
+  } else {
+    std::cout << "  The Root is NULL" << std::endl;
+  }
+  std::cout << "The address of the root's left child is: " << this->root->left << std::endl;
+  if (this->root->left) {
+    std::cout << "  And its value is: " << this->root->left->val << std::endl;
+  } else {
+    std::cout << "  The left child is NULL" << std::endl;
+  }
+  std::cout << "The address of the root's right child is: " << this->root->right << std::endl;
+  if (this->root->right) {
+    std::cout << "  And its value is: " << this->root->right->val << std::endl;
+  } else {
+    std::cout << "  The right child is NULL" << std::endl;
+  }
+  std::cout << std::endl;
 }
--- C++ Projects/Tree/BinaryTree.h
+++ C++ Projects/Tree/BinaryTree.h
@@ -6,6 +6,8 @@
 #ifndef _TREE_BINARY_SEARCH_TREE_H_
 #define _TREE_BINARY_SEARCH_TREE_H_
 #include "Tree.h"
+
+
 template <class T>
 class BinaryTree : public Tree<T> {
  public:
@@ -14,25 +16,26 @@ class BinaryTree : public Tree<T> {
   void Remove(T);
   bool Contains(T) const;
   T Top() const;
+  T Pop();
   void Print() const;
-  void PrintInOrder() const;
-  void PrintPostOrder() const;
+  void PrintTop() const;
   class Node {
    public:
     Node();
     Node(T val);
     Node(T val, Node* left_, Node* right_);
-   private:
     Node* left;
     Node* right;
     T val;
   };
  private:
-  BinaryTree::Node* root;
+  Node* root;
   Node* Insert(Node*, T);
   Node* Remove(Node*, T);
   bool Contains(Node*, T) const;
   void PrintInOrder(Node*) const;
+  Node* FindMinFrom(Node* current);
+  Node* RemoveMinFrom(Node* current);
 };
-#include "BinaryTree.cc"
+#include "../Tree/BinaryTree.cc"
 #endif
