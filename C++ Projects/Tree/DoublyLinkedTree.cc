070018bf1b3fbfabcc26025c0a298d6a09d7b4a9
Aaron Barge <bargeaaron@gmail.com>
2019-03-25 21:36:18 -0400

    Added inefficient depth search

--- C++ Projects/Tree/DoublyLinkedTree.cc
+++ C++ Projects/Tree/DoublyLinkedTree.cc
@@ -1,3 +1,166 @@
 // Written by Aaron Barge
 // Copyright 2019
 
+#ifndef _TREE_DOUBLY_LINKED_TREE_CC_
+#define _TREE_DOUBLY_LINKED_TREE_CC_
+#include <iostream>
+#include "DoublyLinkedTree.h"
+#endif
+/*
+ * Constructors
+ */
+template <class T>
+DLTree<T>::DLTree() {
+  this->root == NULL;
+}
+template <class T>
+DLTree<T>::Node::Node() {
+  this->val = NULL;
+  this->left = NULL;
+  this->right = NULL;
+  this->parent = NULL;
+}
+template <class T>
+DLTree<T>::Node::Node(T val) {
+  this->val = val;
+  this->left = NULL;
+  this->right = NULL;
+  this->parent = NULL;
+}
+template <class T>
+DLTree<T>::Node::Node(T val, DLTree<T>::Node* left, DLTree<T>::Node* right, DLTree<T>::Node* parent) {
+  this->val = val;
+  this->left = left;
+  this->right = right;
+  this->parent = parent;
+}
+
+
+/*
+ * Node addition
+ */
+template <class T>
+void DLTree<T>::Add(T val) {
+  if (this->root == NULL)
+    this->root = new Node(val);
+  Insert(this->root, NULL, val);
+}
+template <class T>
+typename DLTree<T>::Node* DLTree<T>::Insert(DLTree<T>::Node* current, DLTree<T>::Node* previous, T val) {
+  if (current == NULL) {
+    current = new Node(val);
+    current->parent = previous;
+  } else if (val < current->val) {
+    current->left = Insert(current->left, current, val);
+  } else if (val > current->val) {
+    current->right = Insert(current->right, current, val);
+  }
+  return current;
+}
+
+
+/*
+ * Node removal
+ */
+template <class T>
+void DLTree<T>::Remove(T val) {
+  return;
+}
+
+
+/*
+ * Search method
+ */
+template <class T>
+bool DLTree<T>::Contains(T val) const {
+  return BinaryTree<T>::Contains(val);
+}
+
+
+/*
+ * Top method
+ */
+template <class T>
+T DLTree<T>::Top() const {
+  return BinaryTree<T>::Top();
+}
+
+
+/*
+ * Pop off top method
+ */
+template <class T>
+T DLTree<T>::Pop() {
+  return BinaryTree<T>::Pop();
+}
+
+
+/*
+ * Size and Depth methods
+ */
+template <class T>
+size_t DLTree<T>::Size() const {
+  return BinaryTree<T>::Size();
+}
+template <class T>
+size_t DLTree<T>::MaxDepth() const {
+  return BinaryTree<T>::MaxDepth();
+}
+template <class T>
+size_t DLTree<T>::Depth(T val) const {
+  if (this->root == NULL)
+    return -1;
+  return Depth(this->root, val);
+}
+template <class T>
+size_t DLTree<T>::Depth(DLTree<T>::Node* current, T val) const {
+  if (this == NULL)
+    return -1;
+  if (current->val < val)
+    return Depth(current->right, val);
+  if (current->val > val)
+    return Depth(current->left, val);
+  size_t depth = 0;
+  while (current != this->root) {
+    depth++;
+    current = current->parent;
+  }
+  return depth;
+}
+
+
+/*
+ * Print method 
+ */
+template <class T>
+void DLTree<T>::Print() const {
+  return BinaryTree<T>::Print();
+}
+
+
+/*
+ * Print memory locations for top (debugging)
+ */
+template <class T>
+void DLTree<T>::PrintTop() const {
+  std::cout << "The address of the root is: " << this->root << std::endl;
+  if (this->root)
+    std::cout << "  And its value is: " << this->root->val << std::endl;
+  else
+    std::cout << "  And its value is NULL" << std::endl;
+  std::cout << "The address of the root's left child is: " << this->root->left << std::endl;
+  if (this->root->left) {
+    std::cout << "  And its value is: " << this->root->left->val << std::endl;
+    std::cout << "  It is connected to the root" << (this->root->left->parent == this->root ? "TRUE":"FALSE") << std::endl;
+  } else {
+    std::cout << "  The left child is NULL" << std::endl;
+  }
+  std::cout << "The address of the root's right child is: " << this->root->right << std::endl;
+  if (this->root->right) {
+    std::cout << "  It is connected to the root" << (this->root->right->parent == this->root ? "TRUE" : "FALSE") << std::endl;
+    std::cout << "  And its value is: " << this->root->right->val << std::endl;
+  } else {
+    std::cout << "  The right child is NULL" << std::endl;
+  }
+  std::cout << std::endl;
+}
--- C++ Projects/Tree/DoublyLinkedTree.h
+++ C++ Projects/Tree/DoublyLinkedTree.h
@@ -9,25 +9,36 @@
 template <class T>
 class DLTree : public BinaryTree<T> {
  public:
-  DLTree();
-  void Add(T);
-  void Remove(T);
-  bool Contains(T) const;
-  T Top() const;
-  T Pop();
-  void Print() const;
-  void PrintTop() const;
+  DLTree(); // Default tree constructor
+  void Add(T); // Adds a value to the tree
+  void Remove(T); // Removes a value from the tree
+  bool Contains(T) const; // Searches tree for a value
+  T Top() const; // Returns the root's value
+  T Pop(); // Pops the root off and returns value
+  size_t Size() const; // Returns the number of elements in the tree
+  size_t MaxDepth() const; // Returns the maximum depth of the tree
+  size_t Depth(T) const; // Returns the depth of the node with the specified value
+                         // It is good to note that this is fairly inefficient compared to
+                         // It's parent's implementation. Where the parent's implementation
+                         // Keeps track of the current depth while searching this one does not
+                         // and instead just uses the parent pointer to go to the root
+  void Print() const; // Print's the tree's values using an in-order traversal
+  void PrintTop() const; // Method to print the top of the tree's references
   class Node {
    public:
-    Node();
-    Node(T val);
-    Node* left;
-    Node* right;
-    Node* parent;
-    T val;
+     Node(); // Default node constructor
+     Node(T val); // Node constructor with only value
+     Node(T val, Node *left, Node *right, Node *parent); // Explicit Node constructor
+     Node *left;
+     Node *right;
+     Node *parent;
+     T val;
   };
- private:
+
+private: // Helper methods and root
   Node* root;
+  Node* Insert(Node*, Node*, T);
+  size_t Depth(Node*, T) const;
 };
-#include "../Tree/DoublyLinkedTree.cc"
+#include "DoublyLinkedTree.cc"
 #endif
