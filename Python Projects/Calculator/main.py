c3765610427ff0cc4f480b55a1ec6cc0011f6d2b
Aaron Barge <bargeaaron@gmail.com>
2019-03-29 09:52:58 -0400

    Got addition working

--- Python Projects/Calculator/main.py
+++ Python Projects/Calculator/main.py
@@ -1,52 +1,72 @@
 '''
 The goal of this project is to create a fully functional calculator with gui
 '''
-import tkinter
-
+import tkinter as tk
 
 class Calculator(object):
   def __init__(self, width):
-    self.calc = tkinter.Tk()
+    self.prev_operator = '='
+    self.calc = tk.Tk()
     self.width = width
     self.font = ('Courier', 30)
     self.func_font= ('Courier', 20)
-    self.curr_input = tkinter.StringVar()
+    self.curr_input = tk.StringVar()
     self.curr_input.set('0')
-    self.curr_function = tkinter.StringVar()
+    self.curr_function = tk.StringVar()
     self.curr_function.set('')
     self.result = 0
     self.buttons = []
     self.init()
     self.pack()
+    self.bind()
     self.calc.mainloop()
+
+
   def init(self):
     self.calc.title('Calculator')
-    self.buttons.insert(0, [tkinter.Message(self.calc, textvariable=self.curr_function, width=self.width, font=self.func_font)])
-    self.buttons.insert(1, [tkinter.Message(self.calc, textvariable=self.curr_input, width=self.width, font=self.font)])
-    self.buttons.insert(2, [tkinter.Button(self.calc, text='%', width=self.width//4, height=1, font=self.font),
-                            tkinter.Button(self.calc, text='sqrt', width=self.width//4, height=1, font=self.font),
-                            tkinter.Button(self.calc, text='x^2', width=self.width//4, height=1, font=self.font),
-                            tkinter.Button(self.calc, text='1/x', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('='))])
-    self.buttons.insert(3, [tkinter.Button(self.calc, text='CE', width=self.width//4, height=1, font=self.font),
-                            tkinter.Button(self.calc, text='C', width=self.width//4, height=1, font=self.font),
-                            tkinter.Button(self.calc, text='DEL', width=self.width//4, height=1, font=self.font),
-                            tkinter.Button(self.calc, text='/', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('/'))])
-    self.buttons.insert(4, [tkinter.Button(self.calc, text='7', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(7)),
-                            tkinter.Button(self.calc, text='8', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(8)),
-                            tkinter.Button(self.calc, text='9', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(9)),
-                            tkinter.Button(self.calc, text='X', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('*'))])
-    self.buttons.insert(5, [tkinter.Button(self.calc, text='4', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(4)),
-                            tkinter.Button(self.calc, text='5', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(5)),
-                            tkinter.Button(self.calc, text='6', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(6)),
-                            tkinter.Button(self.calc, text='-', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('-'))])
-    self.buttons.insert(6, [tkinter.Button(self.calc, text='1', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(1)),
-                            tkinter.Button(self.calc, text='2', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(2)),
-                            tkinter.Button(self.calc, text='3', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(3)),
-                            tkinter.Button(self.calc, text='+', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('+'))])
-    self.buttons.insert(7, [tkinter.Button(self.calc, text='+/-', width=self.width//4, height=1, font=self.font),
-                            tkinter.Button(self.calc, text='0', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(0)),
-                            tkinter.Button(self.calc, text='.', width=self.width//4, height=1, font=self.font),
-                            tkinter.Button(self.calc, text='=', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('='))])
+    self.buttons.insert(0, [tk.Label(self.calc, textvariable=self.curr_function, width=self.width, font=self.func_font)])
+    self.buttons.insert(1, [tk.Label(self.calc, textvariable=self.curr_input, width=self.width, font=self.font)])
+    self.buttons.insert(2, [tk.Button(self.calc, text='%', width=self.width//4, height=1, font=self.font),
+                            tk.Button(self.calc, text='sqrt', width=self.width//4, height=1, font=self.font),
+                            tk.Button(self.calc, text='x^2', width=self.width//4, height=1, font=self.font),
+                            tk.Button(self.calc, text='1/x', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('='))])
+    self.buttons.insert(3, [tk.Button(self.calc, text='CE', width=self.width//4, height=1, font=self.font, command=self.clear_input),
+                            tk.Button(self.calc, text='C', width=self.width//4, height=1, font=self.font),
+                            tk.Button(self.calc, text='DEL', width=self.width//4, height=1, font=self.font, command=self.clear_last),
+                            tk.Button(self.calc, text='/', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('/'))])
+    self.buttons.insert(4, [tk.Button(self.calc, text='7', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(7)),
+                            tk.Button(self.calc, text='8', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(8)),
+                            tk.Button(self.calc, text='9', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(9)),
+                            tk.Button(self.calc, text='X', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('*'))])
+    self.buttons.insert(5, [tk.Button(self.calc, text='4', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(4)),
+                            tk.Button(self.calc, text='5', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(5)),
+                            tk.Button(self.calc, text='6', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(6)),
+                            tk.Button(self.calc, text='-', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('-'))])
+    self.buttons.insert(6, [tk.Button(self.calc, text='1', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(1)),
+                            tk.Button(self.calc, text='2', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(2)),
+                            tk.Button(self.calc, text='3', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(3)),
+                            tk.Button(self.calc, text='+', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('+'))])
+    self.buttons.insert(7, [tk.Button(self.calc, text='+/-', width=self.width//4, height=1, font=self.font),
+                            tk.Button(self.calc, text='0', width=self.width//4, height=1, font=self.font, command=lambda:self.push_current(0)),
+                            tk.Button(self.calc, text='.', width=self.width//4, height=1, font=self.font),
+                            tk.Button(self.calc, text='=', width=self.width//4, height=1, font=self.font, command=lambda:self.push_function('='))])
+
+
+  def bind(self):
+    self.calc.focus_set()
+    self.calc.bind('KP_7', lambda:self.push_current(7))
+    self.calc.bind('KP_8', lambda:self.push_current(8))
+    self.calc.bind('KP_9', lambda:self.push_current(9))
+    self.calc.bind('KP_4', lambda:self.push_current(4))
+    self.calc.bind('KP_5', lambda:self.push_current(5))
+    self.calc.bind('KP_6', lambda:self.push_current(6))
+    self.calc.bind('KP_1', lambda:self.push_current(1))
+    self.calc.bind('KP_2', lambda:self.push_current(2))
+    self.calc.bind('KP_3', lambda:self.push_current(3))
+    self.calc.bind('KP_0', lambda:self.push_current(0))
+
+
+
   def pack(self):
     for row in range(len(self.buttons)):
       if row == 0 or row == 1:
@@ -54,21 +74,45 @@ class Calculator(object):
       else:
         for column in range(len(self.buttons[row])):
           self.buttons[row][column].grid(column=column, row=row)
+
+
+  def clear_last(self):
+    self.curr_input.set(int(float(self.curr_input.get())//10))
+
+
   def push_current(self, digit):
-    self.curr_input.set(str(float(self.curr_input.get())*10 + int(digit)))
-  def push_function(self, operator):
-    if operator == '+':
-      self.result = self.result + float(self.curr_input.get())
-      self.curr_function.set(self.curr_function.get() + '+' + self.curr_input.get())
+    self.curr_input.set(str(int(self.curr_input.get())*10 + int(digit)))
+
+
+  def clear_input(self):
     self.curr_input.set(0)
 
+
+  def push_function(self, operator):
+    self.curr_function.set(self.curr_function.get() + self.curr_input.get())
+    if self.prev_operator == '=':
+      self.curr_input.set('0')
+      self.curr_function.set(self.result)
+    if self.result == 0:
+      self.result = self.result + float(self.curr_input.get())
+    if operator == '+':
+      self.prev_operator = '+'
+      self.curr_function.set(self.curr_function.get() + ' + ')
+      self.curr_input.set(0)
+    if operator == '=':
+      if self.prev_operator == '+':
+        self.prev_operator = '='
+        self.result = self.result + float(self.curr_input.get())
+        self.curr_function.set(self.result)
+      self.curr_input.set(self.result)
 
 
 def main():
   '''
   The main method
   '''
-  calculator = Calculator(16) 
+  Calculator(16) 
+
 
 if __name__ == "__main__":
   main()
