87c1536b88fe18e1f98997b01aef94eb24cbc968
Aaron Barge <bargeaaron@gmail.com>
2019-03-29 09:54:58 -0400

    Modified file

--- C++ Projects/Tree/TreeTester.cc
+++ C++ Projects/Tree/TreeTester.cc
@@ -18,17 +18,16 @@ bool testAVLTree(Tree<int>* t);
 bool testTTTree(Tree<int>* t);
 
 int main(int argc, char** argv) {
-  Tree<int>* Tree = new BinaryTree<int>();
-  testBinaryTree(Tree);
-  delete Tree;
-  Tree = new DLTree<int>();
-  if (!testDoublyLinkedTree(Tree)) {
-    std::cout << "Doubly Linked Tree testing has failed" << std::endl;
-  }
+  Tree<int>* t = new BinaryTree<int>();
+  testBinaryTree(t);
+  std::cout << std::endl << std::endl;
+  //delete Tree;
+  Tree<int>* t2 = new DLTree<int>();
+  testDoublyLinkedTree(t2);
   return 0;
 }
 bool testBinaryTree(Tree<int>* t) {
-  std::cout << "Testing Binary Search Tree" << std::endl;
+  std::cout << "Testing Doubly Linked Tree" << std::endl;
   bool result = true;
   std::cout << "Adding values..." << std::endl;
   t->Add(5);
@@ -96,5 +95,70 @@ bool testBinaryTree(Tree<int>* t) {
   return result;
 }
 bool testDoublyLinkedTree(Tree<int>* t) {
-  
+  std::cout << "Testing Doubly Linked Tree" << std::endl;
+  bool result = true;
+  std::cout << "Adding values..." << std::endl;
+  t->Add(5);
+  t->Add(4);
+  t->Add(19);
+  t->Add(2);
+  t->Add(7);
+  t->Add(12);
+  t->Add(1);
+  t->Add(3);
+  t->Add(10);
+  t->Add(6);
+  std::cout << "Values added successfully" << std::endl;
+  t->Print();
+  std::cout << "The size of the tree is currently: " << t->Size() << std::endl;
+  std::cout << "The maximum depth of the tree is currentlty: " << t->MaxDepth() << std::endl;
+  if (!t->Contains(5)) {
+    std::cout << "Contains contained value test failed" << std::endl
+        << "  Expected: " << true << std::endl
+        << "  Actual: " << t->Contains(5) << std::endl;
+    result = false;
+  }
+  if (t->Contains(100)) {
+    std::cout << "Contains not contained value test failed" << std::endl
+        << "  Expected: " << false << std::endl
+        << "  Actual: " << t->Contains(100) << std::endl;
+    return false;
+  }
+  std::cout << "Removing 10" << std::endl;
+  t->Remove(10);
+  t->Print();
+  if (t->Contains(10)) {
+    std::cout << "Value removal was unsuccessful" << std::endl
+        << "  Expected: " << false << std::endl
+        << "  Actual: " << t->Contains(10) << std::endl;
+    result = false;
+  }
+  std::cout << "Removing 2" << std::endl;
+  t->Remove(2);
+  t->Print();
+  if (t->Contains(2)) {
+    std::cout << "Value removal was unsuccessful" << std::endl
+              << "  Expected: " << false << std::endl
+              << "  Actual: " << t->Contains(2) << std::endl;
+    result = false;
+  }
+  std::cout << "Removing value not contained in tree" << std::endl;
+  t->Remove(100);
+  t->Print();
+  std::cout << "The size of the tree is currently: " << t->Size() << std::endl;
+  std::cout << "The maximum depth of the tree is currentlty: " << t->MaxDepth() << std::endl;
+  std::cout << "Popping off root" << std::endl;
+  int old_root = t->Pop();
+  t->Print();
+  std::cout << "Making sure root was removed successfully" << std::endl;
+  if (t->Contains(old_root)) {
+    std::cout << "Value removal was unsuccessful" << std::endl
+              << "  Expected: " << false << std::endl
+              << "  Actual: " << t->Contains(old_root) << std::endl;
+    result = false;
+  }
+  std::cout << "The size of the tree is currently: " << t->Size() << std::endl;
+  std::cout << "The maximum depth of the tree is currentlty: " << t->MaxDepth() << std::endl;
+  std::cout << "Doubly Linked Tree testing has " << (result ? "PASSED" : "FAILED") << std::endl;
+  return result;
 }
