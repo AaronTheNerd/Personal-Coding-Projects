de67a47e142f2a5e1c1ab6438bc204f784f5a5a8
Aaron Barge <bargeaaron@gmail.com>
2019-04-24 12:57:48 -0400

    Started

--- C++ Projects/PathFinding/PathFinding.cc
+++ C++ Projects/PathFinding/PathFinding.cc
@@ -1,6 +1,12 @@
 // Written by Aaron Barge
 // Copyright 2019
 
+#include "Maze.h"
+
+void DepthFirst(Maze m);
+void Djikstra(Maze m);
+void AStar(Maze m);
+
 int main(int argc, char** argv) {
     
 }
