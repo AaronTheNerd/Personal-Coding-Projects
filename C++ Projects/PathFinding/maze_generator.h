aa82d5a62dfe529a93f9d0edcf61519f88f1b5d4
Aaron Barge <bargeaaron@gmail.com>
2019-05-13 18:46:58 -0400

    Started work on maze generator

---
+++ C++ Projects/PathFinding/maze_generator.h
@@ -0,0 +1,12 @@
+// Written by Aaron Barge
+// Copyright 2019
+#ifndef _MAZE_GENERATOR_H_
+#define _MAZE_GENERATOR_H_
+#include "Maze.h"
+#include "bitmap_image.hpp"
+#include <string>
+#endif
+
+bitmap_image Generate(int width, int height, std::string name) {
+
+}
