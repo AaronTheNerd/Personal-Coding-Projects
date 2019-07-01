01e267f2014b0a64df8d7e843c680ba7f3d625b8
Aaron Barge <bargeaaron@gmail.com>
2019-07-01 10:50:35 -0400

    Fixed Add()

---
+++ C++ Projects/Tree/Forest/Huffman.cc
@@ -0,0 +1,246 @@
+// Written by Aaron Barge
+// Copyright 2019
+
+
+#ifndef _TREE_HUFFMAN_TREE_CC_
+#define _TREE_HUFFMAN_TREE_CC_
+#include <iostream>
+#include <string>
+#include <queue>
+#include <map>
+#include "Huffman.h"
+#endif
+
+
+/*
+ * Constructors
+ */
+Huffman::Huffman():
+    root(NULL), char_freqs(std::map<std::string, int>()), text(std::string("")) {}
+Huffman::Node::Node():
+    character(std::string("_")), frequency(0), code(std::string("")), left(Edge()), right(Edge()) {}
+Huffman::Node::Node(const std::pair<std::string, int>& pair):
+    character(pair.first), frequency(pair.second), code(std::string("")), left(Edge()), right(Edge()) {}
+Huffman::Node::Node(std::string char_, int val_):
+    character(char_), frequency(val_), code(std::string("")), left(Edge()), right(Edge()) {}
+Huffman::Node::Node(const Huffman::Node& n):
+    character(n.character), frequency(n.frequency), code(n.code), left(n.left), right(n.right) {}
+
+
+/*
+ * Destructor
+ */
+Huffman::~Huffman() {
+  DestroyTree(this->root);
+}
+void Huffman::DestroyTree(Huffman::Node* current) {
+  if (current != NULL) {
+    DestroyTree(current->left.to);
+    DestroyTree(current->right.to);
+    delete current;
+  }
+}
+Huffman::Node::~Node() {
+  this->left.to = NULL;
+  this->right.to = NULL;
+}
+
+
+/*
+ * Node addition
+ */
+void Huffman::Add(std::string text) {
+  this->text += text;
+  this->GetCharFreqs(text);
+  this->Add();
+  this->GetCodes();
+}
+void Huffman::GetCharFreqs(std::string text) {
+  for (unsigned int i = 0; i < text.size(); ++i) {
+    auto result = this->char_freqs.emplace(text[i] + std::string(""), 1);
+    bool new_pair = result.second;
+    if (!new_pair) {
+      auto iterator = result.first;
+      iterator->second = iterator->second + 1;
+    }
+  }
+}
+bool Huffman::comp::operator() (const Node& n1, const Node& n2) const {
+  return n1.frequency > n2.frequency;
+}
+Huffman::Node Huffman::Node::operator=(const std::pair<std::string, int>& pair) {
+  Node* n = new Node(pair.first, pair.second);
+  return *n;
+}
+void Huffman::Add() {
+  std::priority_queue<Huffman::Node, std::vector<Huffman::Node>, Huffman::comp> PQ(this->char_freqs.begin(), this->char_freqs.end());
+  while (PQ.size() > 1) {
+    Huffman::Node n1 = Huffman::Node(PQ.top());
+    PQ.pop();
+    Huffman::Node n2 = Huffman::Node(PQ.top());
+    PQ.pop();
+    Huffman::Node* n = new Huffman::Node(n1.character + n2.character, n1.frequency + n2.frequency);
+    n->left.to = new Node(n1);
+    n->right.to = new Node(n2);
+    PQ.push(*n);
+  }
+  Huffman::Node node = PQ.top();
+  this->root = new Node(node);
+}
+void Huffman::GetCodes() {
+  return this->GetCodes(this->root, std::string(""));
+}
+void Huffman::GetCodes(Huffman::Node* curr, std::string curr_code) {
+  if (curr == NULL) {
+    return;
+  } else if (curr->left.to == NULL && curr->right.to == NULL) {
+    curr->code = curr_code;
+    return;
+  } else {
+    this->GetCodes(curr->left.to, curr_code + std::string("1"));
+    this->GetCodes(curr->right.to, curr_code + std::string("0"));
+  }
+}
+
+
+/*
+ * Node removal
+ */
+void Huffman::Remove(std::string character) {
+  return;
+}
+bool Huffman::Contains(std::string character) const {
+  return Contains(this->root, character);
+}
+bool Huffman::Contains(Huffman::Node* current, std::string character) const {
+  if (current == NULL)
+    return false;
+  if (current->character == character) {
+    return true;
+  } else if (character < current->character) {
+    return Contains(current->left.to, character);
+  } else {
+    return Contains(current->right.to, character);
+  }
+}
+
+
+/*
+ * Top method
+ */
+std::string Huffman::Top() const {
+  if (this->root)
+    return this->root->character;
+  else
+    return NULL;
+}
+
+
+/*
+ * Pop off top method
+ */
+std::string Huffman::Pop() {
+  return NULL;
+}
+
+
+/*
+ * Size and Depth methods
+ */
+unsigned int Huffman::Size() const {
+  return this->root->character.size();
+}
+unsigned int Huffman::MaxDepth() const {
+  return MaxDepth(this->root, 0);
+}
+unsigned int Huffman::MaxDepth(Huffman::Node* current, unsigned int curr_depth) const {
+  if (current == NULL)
+    return curr_depth;
+  else {
+    unsigned int left_depth = MaxDepth(current->left.to, curr_depth + 1);
+    unsigned int right_depth = MaxDepth(current->right.to, curr_depth + 1);
+    return (left_depth > right_depth) ? left_depth : right_depth;
+  }
+}
+unsigned int Huffman::Depth(std::string character) const {
+  if (this->root == NULL)
+    return -1;
+  return Depth(this->root, character, 0);
+}
+unsigned int Huffman::Depth(Huffman::Node* current, std::string character, unsigned int curr_depth) const {
+  if (current == NULL)
+    return -1;
+  if (current->character == character)
+    return curr_depth;
+  if (current->character > character)
+    return Depth(current->left.to, character, curr_depth + 1);
+  else
+    return Depth(current->right.to, character, curr_depth + 1);
+}
+
+
+/*
+ * Encoding
+ */
+std::string Huffman::Encode() const {
+
+}
+std::string Huffman::GetCodeOf(std::string character) const {
+  
+}
+
+
+/*
+ * Decoding
+ */
+std::string Huffman::Decode(std::string) const {
+
+}
+
+
+/*
+ * Print method 
+ */
+void Huffman::Print() const {
+  if (this->root == NULL)
+    return;
+  PrintInOrder(this->root);
+  std::cout << std::endl;
+}
+void Huffman::PrintInOrder(Huffman::Node* curr) const {
+  if (curr == NULL) {
+    return;
+  } else if (curr->left.to == NULL && curr->right.to == NULL) {
+    std::cout << "'" << curr->character << "': " << curr->frequency << "| " << curr->code << std::endl;
+    return;
+  } else {
+    PrintInOrder(curr->left.to);
+    PrintInOrder(curr->right.to);
+  }
+}
+
+
+/*
+ * Print memory locations for top (debugging)
+ */
+void Huffman::PrintTop() const {
+  std::cout << "The address of the root is: " << this->root << std::endl;
+  if (this->root != NULL) {
+    std::cout << "  And its character is: '" << this->root->character << "'" << std::endl;
+  } else {
+    std::cout << "  The Root is NULL" << std::endl;
+  }
+  std::cout << "The address of the root's left child is: " << this->root->left.to <<  std::endl;
+  if (this->root->left.to) {
+    std::cout << "  And its character is: '" << this->root->left->character << "'" << std::endl;
+  } else {
+    std::cout << "  The left child is NULL" << std::endl;
+  }
+  std::cout << "The address of the root's right child is: " << this->root->right.to << std::endl;
+  if (this->root->right.to) {
+    std::cout << "  And its character is: '" << this->root->right->character << "'" << std::endl;
+  } else {
+    std::cout << "  The right child is NULL" << std::endl;
+  }
+  std::cout << std::endl;
+}
---
+++ C++ Projects/Tree/Forest/Huffman.h
@@ -0,0 +1,72 @@
+// Written by Aaron Barge
+// Copyright 2019
+
+
+#ifndef _TREE_HUFFMAN_TREE_H_
+#define _TREE_HUFFMAN_TREE_H_
+#include <string>
+#include <map>
+#include "Tree.h"
+
+
+class Huffman : public Tree<std::string> {
+ public:
+  Huffman();                             // Default tree constructor
+  ~Huffman();                            // Tree destructor
+  void Add(std::string);                 // Adds text to the tree
+  void Remove(std::string);              // Does nothing
+  bool Contains(std::string) const;      // Returns true if a character is contained in the character set
+  std::string GetCodeOf(std::string) const; // Returns the code of a character if that character is in the Huffman tree
+  std::string Top() const;               // Returns the entire character set
+  std::string Pop();                     // Does nothing
+  unsigned int Size() const;             // Returns the amount of characters in the character set
+  unsigned int MaxDepth() const;         // Returns the maximum size of an encoded character
+  unsigned int Depth(std::string) const; // Returns the size of the specified encoded character
+  void Print() const;                    // Print's each character's frequency and code
+  std::string Encode() const;            // Returns all the text encoded using Huffman Encoding
+  std::string Decode(std::string) const; // Attempts to Decode a binary string into it's original text based off of current Huffman Codes
+  void PrintTop() const;                 // Print's the entire character set and character count
+ protected:
+  class Node {
+   public:
+    class Edge {
+     public:
+      Node* to;
+      Edge() { to = NULL; }
+      Edge(Node* to_) { to = to_; }
+      ~Edge() { delete(to); }
+      Node* operator->() { return to; }
+      Node& operator*() { return *to; }
+    };
+    std::string character;
+    int frequency;
+    std::string code;
+    Edge left;
+    Edge right;
+    Node();                              // Default node constructor
+    Node(std::string, int);        // Node constructor with only value
+    Node(const std::pair<std::string, int>&);// Node constructor for type conversion
+    Node(const Node&);
+    Node operator=(const std::pair<std::string, int>&);
+    ~Node();                             // Node destructor
+  };                                     // Helper methods and root
+  class comp {
+   public:
+    bool operator() (const Node& n1, const Node& n2) const;
+  };
+  Node* root;
+  std::map<std::string, int> char_freqs;
+  std::string text;
+  void DestroyTree(Node*);
+  void GetCharFreqs(std::string);
+  void Add();
+  void GetCodes();
+  void GetCodes(Node*, std::string);
+  std::string GetCodeOf(Node*, std::string);
+  bool Contains(Node*, std::string) const;
+  unsigned int Size(Node*) const;
+  unsigned int MaxDepth(Node*, unsigned int depth) const;
+  unsigned int Depth(Node*, std::string, unsigned int depth) const;
+  void PrintInOrder(Node*) const;
+};
+#endif
