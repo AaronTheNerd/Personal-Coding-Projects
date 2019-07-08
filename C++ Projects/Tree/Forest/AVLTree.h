631501a2cc2685ff0ef06bb84bc73656ab0302a2
Aaron Barge <bargeaaron@gmail.com>
2019-07-08 01:20:39 -0400

    Finally started work on AVL Tree

---
+++ C++ Projects/Tree/Forest/AVLTree.h
@@ -0,0 +1,41 @@
+// Written by Aaron Barge
+// Copyright 2019
+
+#ifndef _FOREST_AVL_TREE_H_
+#define _FOREST_AVL_TREE_H_
+#include "DoublyLinkedTree.h"
+
+
+template <typename T>
+class AVLTree : public DLTree<T> {
+ public:
+  AVLTree();
+  ~AVLTree();
+  void Add(T);
+  void Remove(T);
+  bool contains(T) const;
+  T Top() const;
+  T Pop();
+  unsigned int Size() const;
+  unsigned int MaxDepth() const;
+  unsigned int Depth() const;
+  void Print() const;
+  void PrintTop() const;
+ protected:
+  class AVLNode : public DLTree<T>::DLNode {
+   public:
+    typedef typename Tree<T>::template Edge<AVLNode> AVLEdge;
+    AVLNode();                      // Default node constructor
+    AVLNode(T);                     // Node constructor with only value
+    AVLNode(T, AVLEdge, AVLEdge, AVLEdge);   // Explicit Node constructor
+    ~AVLNode();                     // Node destructor
+    AVLEdge parent;
+    int weight;
+  };                             // Helper methods and root
+  AVLNode* root;
+  void Left(AVLNode*);
+  void Right(AVLNode*);
+};
+
+#include "AVLTree.cc"
+#endif
