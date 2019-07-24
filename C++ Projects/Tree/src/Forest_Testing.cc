734f4af578d352f702dac12f0877d5b6991e22d9
Aaron Barge <bargeaaron@gmail.com>
2019-07-24 12:42:58 -0400

    Uncommented tests

--- C++ Projects/Tree/src/Forest_Testing.cc
+++ C++ Projects/Tree/src/Forest_Testing.cc
@@ -15,14 +15,17 @@ int main(int argc, char** argv) {
   Tree<int>* t = new BinaryTree<int>();
   BST_TESTER bst;
   bst.testBinaryTree(t);
-  delete t;
   std::cout << std::endl << std::endl << "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+" << std::endl << std::endl;
   t = new DLTree<int>();
   DL_TESTER dl;
   dl.testDLTree(t);
-  delete t;
   std::cout << std::endl << std::endl << "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+" << std::endl << std::endl;
   Huffman* huffman = new Huffman();
   HUFFMAN_TESTER huff;
   huff.testHuffmanTree(huffman);
+  delete huffman;
+  std::cout << std::endl << std::endl << "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+" << std::endl << std::endl;
+  t = new AVLTree<int>();
+  AVL_TESTER avl;
+  avl.testAVLTree(t);
 }
