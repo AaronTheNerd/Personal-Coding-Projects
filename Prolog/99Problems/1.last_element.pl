321db975b345ab376c43bcdbbf5fb5c7bc35c433
AaronTheNerd <bargeaaron@gmail.com>
2020-12-30 10:42:31 -0500

    Fixed some formatting

--- Prolog/99Problems/1.last_element.pl
+++ Prolog/99Problems/1.last_element.pl
@@ -1,5 +1,5 @@
 % Problem 1. Find last element in a list
 last_element(_, []):- fail.
-last_element(X, [X|[]]).
+last_element(X, [X]).
 last_element(X, [_|L]):-
     last_element(X, L).
--- Prolog/99Problems/2.last_but_one.pl
+++ Prolog/99Problems/2.last_but_one.pl
@@ -1,6 +1,6 @@
 % Problem 2. Find the last but one element
 last_but_one(_, []):- fail.
-last_but_one(_, [_|[]]):- fail.
-last_but_one(X, [X,_|[]]).
+last_but_one(_, [_]):- fail.
+last_but_one(X, [X,_]).
 last_but_one(X, [_|L]):-
     last_but_one(X, L).
