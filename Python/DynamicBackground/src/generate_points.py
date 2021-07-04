6e3c67a868dc1872ca4ab9492843500e01210fd2
AaronTheNerd <bargeaaron@gmail.com>
2021-07-04 11:05:54 -0400

    Added comments

--- Python/DynamicBackground/src/generate_points.py
+++ Python/DynamicBackground/src/generate_points.py
@@ -26,10 +26,12 @@ def generate_border_points():
     return border_points
 
 def random_point():
+    global open_simplex
     if random.random() < DRIFTING_CONFIGS["PERCENTAGE"]:
         return DriftingPoint(
             random.uniform(0, CONFIGS["WIDTH"]),
             random.uniform(0, CONFIGS["HEIGHT"]),
+            None,
             random.randint(DRIFTING_CONFIGS["X_MIN"], DRIFTING_CONFIGS["X_MAX"]),
             random.randint(DRIFTING_CONFIGS["Y_MIN"], DRIFTING_CONFIGS["Y_MAX"]))
     else:
@@ -39,18 +41,25 @@ def random_point():
 
 def generate_points():
     global open_simplex
+    # Generate evenly separated border points
     points = generate_border_points()
+    # Find how many points are border points
     border_points_length = len(points)
+    # Add one initial interior point
     points.append(random_point())
+    # Add points while there is space to
     fails = 0
     while len(points) - border_points_length < POINT_CONFIGS["NUM_OF_POINTS"] and fails < POINT_CONFIGS["MAX_FAILS"]:
+        # Create a new point
         new_point = random_point()
         failed = False
+        # Check if the point can fit without being too close to other points
         for point in points:
             if (point.x - new_point.x)**2 + (point.y - new_point.y)**2 < POINT_CONFIGS["SEPARATION_RADIUS"]**2:
                 fails += 1
                 failed = True
                 break
+        # Add new point to total points if the point is well separated from others
         if not failed:
             points.append(new_point)
             fails = 0
