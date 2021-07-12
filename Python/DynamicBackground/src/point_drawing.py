d2f7456b36ae3a47ad4434e320a47f14e8435311
AaronTheNerd <bargeaaron@gmail.com>
2021-07-12 07:41:56 -0400

    Modified init function to use KWARGS attribute

--- Python/DynamicBackground/src/point_drawing.py
+++ Python/DynamicBackground/src/point_drawing.py
@@ -1,7 +1,7 @@
 class PlainPoint(object):
-    def __init__(self, color, width):
-        self.color = color
-        self.width = width
+    def __init__(self, COLOR=None, WIDTH=1):
+        self.color = COLOR if COLOR is not None else [0] * 3
+        self.width = WIDTH
     def get_color(self, x, y):
         return self.color
     def get_width(self, x, y):
